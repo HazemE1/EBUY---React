f3a370ccc3fac13bd68b6bd2a6958f2f62477618
Sahand <sahand.prs@gmail.com>
2022-05-05 09:47:51 +0200

    Still working on the front

--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,45 @@
-import React from "react"
-import "./Product.css"
+import React from "react";
+import "./Product.css";
+//import { useStateValue } from "./StateProvider";
 
-function Product() {
+function Product({id, title, image, price, rating}) {
+
+    const [state, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item: {
+                id: id,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    };
+
     return (
-        <div>
-            <div className="Product">
-                <div className="product__info">
-                    <p>Title</p>
-                    <p className="product__price">€30</p>
-                    <p className="product__rating">⭐️⭐️⭐️</p>
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating)
+                    .fill()
+                    .map((_, i) => (
+                        <p>⭐</p>
+                    ))}
                 </div>
-                <img src="import imageImport from './image.png';" alt="" />
             </div>
 
-            <button>Add to Basket</button>
+            <img src={image} />
 
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
+
 export default Product
